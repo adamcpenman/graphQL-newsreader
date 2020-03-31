8a3ab8ca311191511ba958d5413807e8901ca7e0
Adam <adamcpenman@gmail.com>
2020-03-31 16:24:10 -0500

    resolvers

--- resolvers/index.js
+++ resolvers/index.js
@@ -1,5 +1,15 @@
 export const resolvers = {
   Query: {
+    allArticles: (_, args, { dataSources }) =>
+      Promise.all(
+        Object.keys(dataSources).map(source =>
+          dataSources[source].getAllArticles()
+        )
+      ).then(result => result.reduce((acc, data) => acc.concat(data), [])),
+
+    allArticlesBySource: (_, { source }, { dataSources }) =>
+      dataSources[source].getAllArticles(),
+
     articleBySource: (_, { id, source }, { dataSources }) =>
       dataSources[source].getArticle(id, source),
 
