034dc8b50bb5354b9757253d45243ce8773e0db5
Adam <adamcpenman@gmail.com>
2020-03-31 16:05:33 -0500

    typeDefs

---
+++ typeDefs/index.js
@@ -0,0 +1,17 @@
+import { gql } from 'apollo-server';
+
+export const typeDefs = gql`
+  type Query {
+    articleBySource(id: ID!, source: String!): Article
+    articlesBySource(ids: [Int!], source: String!): [Article!]!
+  }
+
+  type Article {
+    id: ID!
+    title: String
+    author: String
+    url: String
+    time: String
+    source: String
+  }
+`;
